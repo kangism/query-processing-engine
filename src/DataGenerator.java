4bc74a7c3569e6d37e11f903b7f0783e0293fc29
kangism <kangism@97d403de-bac6-683a-bfd6-53bccf9f65de>
2010-10-25 06:31:08 +0000

    generate Data for 5 tables. each table has 1500 tuples

---
+++ src/DataGenerator.java
@@ -0,0 +1,55 @@
+import java.io.IOException;
+
+/**
+ * 
+ */
+
+/**
+ * @author workshop
+ *
+ */
+public class DataGenerator {
+
+	/**
+	 * 
+	 */
+	public DataGenerator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void generator(String dir)
+	{
+		String table_schema[]={"Flights","Aircrafts","Employees","Certified","Schedule"};
+		int numtuples[]={1500,1500,1500,1500,1500};
+		int len=table_schema.length;
+		
+		RandomDB rand=new RandomDB();
+		for(int i=0;i<len;i++)
+		{
+			
+			table_schema[i]=dir+"\\"+table_schema[i];
+			System.out.println("Start generating table "+table_schema[i]+" ...");
+			rand.randomGenerator(table_schema[i], numtuples[i]);
+			try {
+				ConvertTxtToTbl.convert(table_schema[i]);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.out.println(e.getMessage());
+			}
+			System.out.println("Table "+table_schema[i]+" generated successfully!");
+		}		
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String dir="C:\\workspace\\sql";
+		DataGenerator datag=new DataGenerator();
+		datag.generator(dir);
+		
+
+	}
+
+}
